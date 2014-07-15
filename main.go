5c6b85daf1d460b129ebdd24fcfe015cf4539709
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-07-15 15:27:05 -0700

    messing with channels

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"foo/config"
-	"github.com/go-martini/martini"
 	"fmt"
+	"time"
 )
 
-func main() {
-	config.AppConfiguration()
+var c = make(chan int, 5)
 
-  m := martini.Classic()
-  m.Post("/foo", func(params martini.Params) string {
-        fmt.Println(params)
-    return "Hello world!"
-  })
-  m.Run()
+func main() {
+	go worker(1)
+	for i := 0; i < 10; i++ {
+		c <- i
+		fmt.Println(i)
+	}
+}
+
+func worker(id int) {
+	for {
+		_ = <-c
+		time.Sleep(time.Second)
+	}
 }
