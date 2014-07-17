002b0b61057105b10d9eceaf29a9644ccf7f7380
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-07-16 17:34:25 -0700

    Re-enabling json, uuid, s3 (s3 will only work if valid env variables are exported

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This is me experimenting with golang
+
+````
+# git clone <giturl>
+# export GOPATH="$HOME/.go" # or wherever
+cd gosperiments
+
+go get
+go build main.go
+./main
+````
--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	// "foo/json"
-	// "foo/s3"
-	// "foo/uuid"
+	"foo/json"
+	"foo/s3"
+	"foo/uuid"
 	"foo/batch"
 )
 
 func main() {
-	// uuid.GenerateUuid()
+	uuid.GenerateUuid()
 
-	// json.ParseKnownFormatJsonFromFile()
-	// json.ParseArbitraryJson()
-	// json.ParseArbitraryJsonUsingLibrary()
-	// json.ParseArbitraryJsonFromFileUsingLibrary()
-	// json.WriteJsonToFile()
+	json.ParseKnownFormatJsonFromFile()
+	json.ParseArbitraryJson()
+	json.ParseArbitraryJsonUsingLibrary()
+	json.ParseArbitraryJsonFromFileUsingLibrary()
+	json.WriteJsonToFile()
 
-	// // see buckets at https://console.aws.amazon.com/s3/home?region=us-west-2
-	// bucketName := "someNewBuckete88c2f9e-b081-4f72-8559-13b9e8001d48" // existing bucket created via fmt.Sprintf("someNewBucket%s", uuid.GenerateUuid())
-	// s3.UploadSampleFile(bucketName, "somewhereInS3Bucket", []byte("some data to put in the file\nline two"))
+	// see buckets at https://console.aws.amazon.com/s3/home?region=us-west-2
+	bucketName := "someNewBuckete88c2f9e-b081-4f72-8559-13b9e8001d48" // existing bucket created via fmt.Sprintf("someNewBucket%s", uuid.GenerateUuid())
+	s3.UploadSampleFile(bucketName, "somewhereInS3Bucket", []byte("some data to put in the file\nline two"))
 
 	batch.WriteToFileAfterReceivingMessages(5)
 }
